40af716ac45a3eb5b03abccd3d67ca1a2d4ac229
Samir Tiwari <samirtiwari2061@gmail.com>
2024-04-09 18:57:52 +0545

    add useCallback

--- src/components/SelectableGrid.jsx
+++ src/components/SelectableGrid.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 const SelectableGrid = ({ rows = 15, cols = 10 }) => {
   const [isMouseDown, setisMouseDown] = useState(false);
@@ -9,38 +9,41 @@ const SelectableGrid = ({ rows = 15, cols = 10 }) => {
     setselectedBoxes([boxNumber]);
   };
 
-  const handleMouseEnter = (boxNumber) => {
-    if (isMouseDown) {
-      const startBox = selectedBoxes[0];
-      const endBox = boxNumber;
+  const handleMouseEnter = useCallback(
+    (boxNumber) => {
+      if (isMouseDown) {
+        const startBox = selectedBoxes[0];
+        const endBox = boxNumber;
 
-      const startRow = Math.floor((startBox - 1) / cols);
-      const startCol = (startBox - 1) % cols;
-      const endRow = Math.floor((endBox - 1) / cols);
-      const endCol = (endBox - 1) % cols;
+        const startRow = Math.floor((startBox - 1) / cols);
+        const startCol = (startBox - 1) % cols;
+        const endRow = Math.floor((endBox - 1) / cols);
+        const endCol = (endBox - 1) % cols;
 
-      const minRow = Math.min(startRow, endRow);
-      const maxRow = Math.max(startRow, endRow);
-      const minCol = Math.min(startCol, endCol);
-      const maxCol = Math.max(startCol, endCol);
+        const minRow = Math.min(startRow, endRow);
+        const maxRow = Math.max(startRow, endRow);
+        const minCol = Math.min(startCol, endCol);
+        const maxCol = Math.max(startCol, endCol);
 
-      const selected = [];
-      if (startBox < endBox) {
-        for (let row = minRow; row <= maxRow; row++) {
-          for (let col = minCol; col <= maxCol; col++) {
-            selected.push(row * cols + col + 1);
+        const selected = [];
+        if (startBox < endBox) {
+          for (let row = minRow; row <= maxRow; row++) {
+            for (let col = minCol; col <= maxCol; col++) {
+              selected.push(row * cols + col + 1);
+            }
+          }
+        } else {
+          for (let row = maxRow; row >= minRow; row--) {
+            for (let col = maxCol; col >= minCol; col--) {
+              selected.push(row * cols + col + 1);
+            }
           }
         }
-      } else {
-        for (let row = maxRow; row >= minRow; row--) {
-          for (let col = maxCol; col >= minCol; col--) {
-            selected.push(row * cols + col + 1);
-          }
-        }
+        setselectedBoxes(selected);
       }
-      setselectedBoxes(selected);
-    }
-  };
+    },
+    [isMouseDown]
+  );
   const handleMouseUp = () => {
     setisMouseDown(false);
   };
